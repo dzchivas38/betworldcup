0dd34bc0b4c7e46575cc42a5c5d64d766255bce7
chivas38 <nguyendunghd1993@gmail.com>
2017-07-25 07:55:21 +0700

    create something sidebar

--- public/app.js
+++ public/app.js
@@ -9,6 +9,14 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: 'template/Content/partial-home.html'
         })
+        .state('player', {
+            url: '/player',
+            templateUrl: 'template/Content/player-list.html'
+        })
+        .state('action-type', {
+            url: '/action-type',
+            templateUrl: 'template/Content/action-type-list.html'
+        })
         .state('syntax', {
             url: '/syntax',
             templateUrl: 'template/Content/syntax-list.html'
