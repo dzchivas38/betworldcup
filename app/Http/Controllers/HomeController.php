7162e20c10b51db6b751abb2d5c9c5853caf73d3
DungNV44 <dungnv44@fsoft.com.vn>
2017-08-09 19:12:21 +0700

    update datetime

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Awjudd\FeedReader;
 use App\Http\Requests;
 use DOMDocument;
 use DateTime;
 use PhpParser\Node\Expr\Cast\Object_;
 use App\Models\ResultNumber;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -24,16 +24,21 @@ class HomeController extends Controller
         $rss_array = array();
         $items = array();
         $result_net = new ResultNumber();
+        $born = Carbon::createFromDate(1987, 4, 23);
+        $noCake = Carbon::createFromDate(2014, 9, 26);
+        var_dump($born->isBirthday($noCake));
         foreach($doc-> getElementsByTagName($rss_item_tag) AS $node) {
             foreach($rss_tags AS $key => $value) {
                 $items[$value] = $node->getElementsByTagName($value)->item(0)->nodeValue;
             }
             array_push($rss_array, $items);
         }
-        $test = new DateTime("Tue, 08 Aug 2017 14:50:50 GMT",null);
-        //$test->format('YY/dd/mm');
-
-        return json_encode($test);
+//        foreach ($rss_array as $key){
+//            var_dump($key['pubDate']);
+//            break;
+//        }
+//        $test = new DateTime("Tue, 08 Aug 2017 14:50:50 GMT",null);
+//        return json_encode($test);
         //dd($rss_array);
         //return $rss_array;
     }
