f17a28588ec569c7a1269c38666740408c346270
langthuan <nguyendunghd1993@gmail.com>
2018-03-08 21:07:12 +0700

    20180308

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ use App\Http\Requests;
 use DOMDocument;
 use DateTime;
 use DB;
-use Psy\Util\Str;
 
 
 class HomeController extends Controller
@@ -24,6 +23,7 @@ class HomeController extends Controller
     {
         return $this->temp;
     }
+
     public function test()
     {
         $str = "De 01.10.17.71x100n. 56.65x200n. 02.20.26.62.00x100n. de 67.76x400n. 09.90x200n. 25.52x300n. 08.80x100n. 37.73x200n. 58.85x900n. 33x200n. 57.75x100n. 55.77x100n.tin 5";
@@ -38,29 +38,36 @@ class HomeController extends Controller
     private function dequy($str)
     {
         $str = (is_string($str)) ? $str : "";
-        var_dump($this->temp);
-        echo "------------------------------------";
-        $arr1 = array("a", "lo","De");
+        $str = trim($str);
+
+        $arr1 = array("a", "lo", "De");// mảng cú pháp
         $map = new HashMap($arr1);
         if (strlen($str) == 0) {
             return 0;
         } else {
+            echo "---------------str>0---------------------";
             $str1 = new String($str);
             $first = $str1->firstIndexOf(' ');
             $strCheck = $str1->subString($first);
             $checked = $map->hasLike($strCheck);
-            if ($checked){
-                if (count($this->temp)==0){
-                    $this->temp = array($strCheck=>$str1);
-                }else{
-                    //bỏ phần từ mảng cuối cùng
-                    $itemRemoved = array_pop($this->temp);
-                    $arrMerge = array($strCheck=>$str1);
-                    $this->temp = array_merge($itemRemoved,$arrMerge);
+            //Nếu đoạn string sau space đầu tiên có trong mảng cú pháp
+            if ($checked) {
+                if (count($this->temp) == 0) {
+                    $this->temp = array($strCheck => $str1->get());
+                    $this->dequy($str1->get());
+                } else {
+
                 }
-                $this->dequy($str1->get());
-            }else{
-                return 0;
+
+            } else {
+                if ($str1->get() !== str_replace(' ', '', $str1->get())) {
+                    //Have whitespace
+                    $this->dequy($str1->get());
+                } else {
+                    //dont have whitespace
+                    return 0;
+                }
+
             }
         }
     }
