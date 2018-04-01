2b3b29fae211ce0f8bffc1ca1755c79a057f432c
langthuan <nguyendunghd1993@gmail.com>
2018-04-01 14:43:45 +0700

    20180401Sang

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\HashMap;
 use App\Models\Messenger;
+use App\Models\ResultViewModel;
 use App\Models\String;
 use App\Models\Syntax;
 use Illuminate\Http\Request;
@@ -22,7 +23,7 @@ class HomeController extends Controller
     }
     public function test()
     {
-        $str = "De 01.10.17.71x100n. 56.65x200n. 02.20.26.62.00x100n. de 67.76x400n. 09.90x200n. 25.52x300n. 08.80x100n. 37.73x200n. 58.85x900n. Lo 33x200n. 57.75x100n";
+        $str = "De 01.45.91.17.71x100n. 56.65x200n. 02.20.26.62.00x100n. de 67.76x400n. 09.90x200n. 25.52x300n. 08.80x100n. 37.73x200n. 58.85x900n. Lo 33x200n. 57.75x100n";
         $msg = new Messenger($str);
         $syntax = new Syntax();
         $syntaxList = $syntax->getAll();
@@ -31,7 +32,7 @@ class HomeController extends Controller
         }, $syntaxList);
         $msgWithSyntax = $msg->getArrayTobeConvertFromMsg($listName);
         try {
-            $results = DB::table('tbl_result_number')->whereDate('PubDate', '=', '2018-03-24')->first();
+            $results = DB::table('tbl_result_number')->whereDate('PubDate', '=', '2018-03-31')->first();
         } catch (\Exception $e) {
             return $e->getMessage();
         }
@@ -52,42 +53,126 @@ class HomeController extends Controller
     public function checkLive($tin_nhan,$cash_out,$ket_qua_so_xo){
         $syn = New Syntax();
         $hm = new HashMap($cash_out);
-
+        $vm_result_list = array();
+        $kq_list = array_map(function($key,$value) {
+            $value = trim($value);
+            $i = strpos($value, " ",0);
+            $v = substr($value,$i,strlen($value) - $i);
+            $v = trim($v);
+            if ($key == 0){
+                return $v;
+            }else{
+                $v = explode(" - ",$v);
+                return $v;
+            }
+        }, array_keys($ket_qua_so_xo),array_values($ket_qua_so_xo));
         foreach ($tin_nhan as $key=>$value){
             $criteria = $syn->getConstSyn($key);
             $syntaxForm = $criteria->SyntaxForm;
             $x = $hm->findByKey($syntaxForm,'Code');
             $cus_cash_out = array_pop($x);
-            $this->parserMsg($value,$cus_cash_out);
+            $vm_result = $this->parserMsg($value,$cus_cash_out,$kq_list);
+            $vm_result_list = array_merge($vm_result_list,$vm_result);
         }
-        //dd($t->SyntaxForm);
+        return $vm_result_list;
     }
-    //hàm tính đoạn tin nhắn con trả về giá trị tổng.
-    public function parserMsg($tin_nhan,$cash_out){
-        $sum_value = 0;
+    //hàm tính đoạn tin nhắn con trả mảng kết quả.
+    public function parserMsg($tin_nhan,$cash_out,$ket_qua_so_xo){
+        $return_list = array();
         $tin_nhan =  new String($tin_nhan);
         $tin_nhan->trimS();
         $point_group = $tin_nhan->explode(" ");
         if(count($point_group) > 0){
             foreach ($point_group as $key=>$value){
                 $str = new String($value);
+                $obj_result_vm = new ResultViewModel($value);
                 $index = $str->firstIndexOf("x");
-                $poit_vali = $str->subString($index);
+                $point_vali = $str->subString($index);
+                $num_list = explode(".",$point_vali);
+
                 //chuyển chuỗi string thành mảng
                 $char_list_money = str_split($str->get());
                 $n = $this->getFirstIndexOfNumber($char_list_money);
                 $c = $this->getFirstIndexOfChar($char_list_money,$n);
                 $vali = substr($str->get(),$n,$c-$n);//giá trị của các số dánh
-                $sum = substr_count($poit_vali,".") + 1; //đếm số đánh qua dấu . nên + 1 do ko có dấu . cuối
+                $sum = count($num_list); //đếm số đánh qua dấu . nên + 1 do ko có dấu . cuối
                 $sum = $sum*$vali*($cash_out->InCoin);
-                dd($sum);
+                // Đoạn so kết quả với từng loại chơi check ở đây
+                if ($cash_out->Code =="de"){
+                    $bingo_list = $this->getResultFormRList($num_list,$ket_qua_so_xo,2,false,0);
+                }else{
+                    $bingo_list = $this->getResultFormRList($num_list,$ket_qua_so_xo,2,true,0);
+                }
+
+                $bingo_count = count($bingo_list);
+                if ($bingo_count >0){
+                    $bingo_val = $bingo_count*$vali*($cash_out->OutCoin);
+                }else{
+                    $bingo_val = 0;
+                }
+                $kq_cc = $bingo_val - $sum;
+                $obj_result_vm->setSum($sum);
+                $obj_result_vm->setSoTrung($bingo_list);
+                $obj_result_vm->setBingo($bingo_val);
+                $obj_result_vm->setKqCc($kq_cc);
+                $obj_result_vm->setActionType($cash_out->Code);
+                $obj_result_vm->setSoDanh($num_list);
+                $return_list[] = $obj_result_vm;
             }
         }
+        return $return_list;
     }
     //Hàm tính giá trị trúng từ kết quả xổ số
     public function getReturnValue(){
 
     }
+    //hàm tìm số trong mảng kết quả dạng arr(arr) trả về mảng những số có trong danh sách
+    //r_list là mảng kết quả xổ số đã gia công
+    private function getResultFormRList($num_list,$r_list,$offset,$check_all,$vi_tri_check){
+        $re_list = array();
+        if ($check_all){
+            foreach ($num_list as $num){
+                foreach ($r_list as $result){
+                    if (is_array($result)){
+                        foreach ($result as $r){
+                            $d = substr($r,strlen($r) - $offset,$offset);
+                            if ($num == $d){
+                                $re_list[] = $num;
+                            }
+                        }
+                    }else{
+                        $d = substr($result,strlen($result) - $offset,$offset);
+                        if ($num == $d){
+                            $re_list[] = $num;
+                        }
+                    }
+                }
+            }
+        }else{
+            $item = "";
+            if (!is_array($r_list[$vi_tri_check])){
+                $res = $r_list[$vi_tri_check];
+                $item = substr($res,strlen($res) - $offset,$offset);
+            }
+            foreach ($num_list as $num){
+                if (is_array($r_list[$vi_tri_check])){
+                    $res = $r_list[$vi_tri_check];
+                    foreach ($res as $r){
+                        $d = substr($r,strlen($r) - $offset,$offset);
+                        if ($num == $d){
+                            $re_list[] = $num;
+                        }
+                    }
+                }else{
+                    if ($num == $item){
+                        $re_list[] = $num;
+                    }
+                }
+            }
+        }
+        return $re_list;
+    }
+
     //hàm lấy vị trị giá trị đầu tiên là số
     private function getFirstIndexOfNumber($arr){
         if (is_array($arr)){
---
+++ app/Models/ResultViewModel.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dev-1
+ * Date: 01/04/2018
+ * Time: 13:37
+ */
+
+namespace App\Models;
+
+
+class ResultViewModel
+{
+    private $msg;
+    private $sum;
+    private $bingo;
+    private $so_danh;
+    private $so_trung;
+    private $kq_cc;
+    private $action_type;
+
+    /**
+     * ResultViewModel constructor.
+     * @param $msg
+     */
+    public function __construct($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    /**
+     * @param mixed $msg
+     */
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSum()
+    {
+        return $this->sum;
+    }
+
+    /**
+     * @param mixed $sum
+     */
+    public function setSum($sum)
+    {
+        $this->sum = $sum;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBingo()
+    {
+        return $this->bingo;
+    }
+
+    /**
+     * @param mixed $bingo
+     */
+    public function setBingo($bingo)
+    {
+        $this->bingo = $bingo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoDanh()
+    {
+        return $this->so_danh;
+    }
+
+    /**
+     * @param mixed $so_danh
+     */
+    public function setSoDanh($so_danh)
+    {
+        $this->so_danh = $so_danh;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoTrung()
+    {
+        return $this->so_trung;
+    }
+
+    /**
+     * @param mixed $so_trung
+     */
+    public function setSoTrung($so_trung)
+    {
+        $this->so_trung = $so_trung;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKqCc()
+    {
+        return $this->kq_cc;
+    }
+
+    /**
+     * @param mixed $kq_cc
+     */
+    public function setKqCc($kq_cc)
+    {
+        $this->kq_cc = $kq_cc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActionType()
+    {
+        return $this->action_type;
+    }
+
+    /**
+     * @param mixed $action_type
+     */
+    public function setActionType($action_type)
+    {
+        $this->action_type = $action_type;
+    }
+
+    public function jsonSerialize() {
+        return [
+            'msg' => $this->msg,
+            'sum' => $this->sum,
+            'bingo' => $this->bingo,
+            'so_danh' => $this->so_danh,
+            'so_trung' => $this->so_trung,
+            'kq_cc' => $this->kq_cc,
+            'action_type'=>$this->action_type
+        ];
+    }
+}
